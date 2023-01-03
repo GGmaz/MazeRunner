69e836baac517f09d2d37dffcf990212f2f939dd
GGmaz <kajtes.nikola@gmail.com>
2023-01-03 12:31:56 +0100

    initial setup

---
+++ MazeRunner/target/debug/MazeRunner.d
@@ -0,0 +1,1 @@
+E:\Fax\Master\Rust\Projekat\MazeRunner\MazeRunner\target\debug\MazeRunner.exe: E:\Fax\Master\Rust\Projekat\MazeRunner\MazeRunner\src\main.rs
---
+++ MazeRunner/target/debug/deps/MazeRunner-6a0b6b0cf8f20a1d.d
@@ -0,0 +1,5 @@
+e:\Fax\Master\Rust\Projekat\MazeRunner\MazeRunner\target\debug\deps\MazeRunner-6a0b6b0cf8f20a1d.rmeta: src\main.rs
+
+e:\Fax\Master\Rust\Projekat\MazeRunner\MazeRunner\target\debug\deps\MazeRunner-6a0b6b0cf8f20a1d.d: src\main.rs
+
+src\main.rs:
